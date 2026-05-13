6d539db4144dfd8c5ca5abbab420f98a754e92d5
Ikhwan Ishak <ikhwanishak2001@gmail.com>
2026-05-13 23:01:59 +0800

    feat: Add CSP and strict security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,33 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data: https:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    connect-src 'self' https://dev.toyyibpay.com;
+`;
+
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false, // Fix: Server Leaks Information via "X-Powered-By"
   async headers() {
     return [
       {
         source: '/(.*)',
         headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload',
+          },
           {
             key: 'X-Frame-Options',
             value: 'DENY',
